33c297b48539ac2b73729267e44853b28eabe0c3
sam <sam@freighttrust.com>
2021-03-27 17:03:07 -0700

    feat(container): docker

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import uvicorn
 from fastapi import FastAPI
+# from api.v1.api import api_router
+import config
 
 from app.api import api
 from app.database import database
 
 app = FastAPI()
 app.include_router(api)
-
+#app.include_router(api_router)
 
 @app.on_event("startup")
 async def startup():
@@ -15,3 +18,7 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=config.SERVER_IP, port=config.SERVER_PORT)
+
